dc4b893d2a052a041b226e0bbe9ad85db034a716
Ahmed Ganna <andy.ahmedg@gmail.com>
2015-10-08 22:28:43 +0200

    New Parse Constants

--- app/src/main/java/com/ganna/faceparse/Constants/ParseConstants.java
+++ app/src/main/java/com/ganna/faceparse/Constants/ParseConstants.java
@@ -4,14 +4,21 @@ package com.ganna.faceparse.Constants;
  * Created by Ahmed on 05/09/2015.
  */
 public class ParseConstants {
-    public static final String JOKES_CLASS ="Jokes";
-    public static final String JOKES_LIKES_CLASS ="Joke_likes";
-    public static final String USER_POINTER ="user_id";
-    public static final String JOKE_POINTER ="joke_id";
-    public static final String LIKES_COLUMN ="likes";
-    public static final String DESCRIPTION_COLUMN ="description";
-    public static final String IMAGE_COLUMN ="image";
-    public static final String DISLIKES_COLUMN ="dislikes";
-    public static final String INTERACTION_COLUMN = "interaction";
+    // Jokes table constants
     public static final String ID_COLUMN = "objectId";
+    public static final String JOKES_CLASS ="Jokes";
+    public static final String LIKES_COLUMN ="likes";
+    public static final String DISLIKES_COLUMN ="dislikes";
+    public static final String TEXT_COLUMN ="text";
+    public static final String IMAGE_COLUMN ="image";
+
+    // user_joke table constants
+    public static final String JOKES_LIKES_CLASS ="user_joke";
+    public static final String USER_POINTER ="user";
+    public static final String JOKE_POINTER ="joke";
+    public static final String INTERACTION_COLUMN = "interaction";
+    public static final String SAVED_COLUMN = "saved";
+    public static final String LIKE_COLUMN = "like";
+    public static final String REPEATED_COLUMN = "repeated";
+    public static final String REPORTED_COLUMN = "reported";
 }
